a95ba660a0f7dcd170d8169a5ea72d87170a9cc7
Sam Mousa <sam@mousa.nl>
2013-09-04 15:46:48 +0200

    Dev Fixed selection CSS. Dev Created form element that creates datatable checkbox list.

---
+++ DataTableCheckboxList.php
@@ -0,0 +1,45 @@
+<?php
+	require_once(__DIR__ . '/DataTable.php');
+	class DataTableCheckboxList extends CInputWidget
+	{
+
+		public $checkBoxColumn;
+		public $options;
+		public $errorOptions;
+
+		public $multiple = true;
+
+
+		public function init() {
+			parent::init();
+			$this->checkBoxColumn = array_merge(array(
+				'class' => 'CCheckBoxColumn'
+			), $this->checkBoxColumn);
+			
+			
+			if ($this->multiple)
+			{
+				$this->options['selectableRows'] = 2;
+			}
+			else
+			{
+				$this->options['selectableRows'] = 1;
+			}
+
+			CHtml::resolveNameID($this->model, $this->attribute, $this->htmlOptions);
+			
+			if(substr($this->htmlOptions['name'],-2)!=='[]')
+			{
+				$this->htmlOptions['name'] .= '[]';
+			}
+
+			$this->checkBoxColumn['checkBoxHtmlOptions']['name'] = $this->htmlOptions['name'];
+			$this->options['columns'][] = $this->checkBoxColumn;
+		}
+		public function run()
+		{
+			$this->widget('DataTable', $this->options);
+			//print_r($this->options);
+		}
+	}
+?>
--- assets/js/widget.js
+++ assets/js/widget.js
@@ -2,7 +2,61 @@
  * Add some extra functionality for easier usage within Yii.
  */
 $(document).ready(function() {
-	$('body').on('click', 'a.removerow', function() {
-		$(this).closest('table').dataTable().fnDeleteRow($(this).closest('tr'));
+	$('body').on('click', 'table.dataTable a.removerow', function() {
+		$(this).closest('table').dataTable({"bRetrieve" : true}).fnDeleteRow($(this).closest('tr'));
 	});
+	$('body').on('click', 'table.dataTable.singleSelect tbody tr', function() {
+		if (!$(this).hasClass('selected'))
+		{
+			$(this).parent().children().removeClass('selected');
+			$(this).addClass('selected');
+			$(this).find('input.select-on-check').attr('checked', true);
+		}
+		else
+		{
+			$(this).removeClass('selected');
+		}
+	});
+	$('body').on('click', 'table.dataTable.multiSelect tbody tr', function() {
+		if (!$(this).hasClass('selected'))
+		{
+			$(this).find('input.select-on-check').attr('checked', true);
+			$(this).addClass('selected');
+		}
+		else
+		{
+			$(this).removeClass('selected');
+			$(this).find('input.select-on-check').attr('checked', false);
+		}
+	});
+	// Don't propagate click events for inputs.'
+	$('body').on('click', 'table.dataTable.multiSelect tbody tr input', function(e) {
+		e.stopPropagation();
+	});
+	$('body').on('click', 'table.dataTable.multiSelect tbody tr input.select-on-check', function(e) {
+		e.stopPropagation();
+		if (this.checked)
+		{
+			$(this).closest('tr').addClass('selected');
+		}
+		else
+		{
+			$(this).closest('tr').removeClass('selected');
+		}
+	});
+
+	$('body').on('click', 'table.dataTable.multiSelect thead tr input.select-on-check-all', function(e) {
+		e.stopPropagation();
+		if (this.checked)
+		{
+			$(this).closest('table').find('tbody tr').addClass('selected');
+			$(this).closest('table').find('tbody tr input.select-on-check').attr('checked', true);
+		}
+		else
+		{
+			$(this).closest('table').find('tbody tr').removeClass('selected');
+			$(this).closest('table').find('tbody tr input.select-on-check').attr('checked', false);
+		}
+	});
+
 });
