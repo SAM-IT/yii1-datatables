add2b3cef7bddf4d8cd38440733e835f271d3a2b
Sam Mousa <sam@mousa.nl>
2014-07-18 11:59:00 +0200

    Check for existence of Befound library.

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -204,28 +204,31 @@ jQuery.extend( jQuery.fn.dataTableExt.oSort, {
     }
 } );
 
-Befound.ready(function() {
-	var ns = Befound.App.name;
-	$(document).on('select.' + ns, function(event, model, id) {
-		var selector = 'table.dataTable[data-basemodel=' + model + '][data-route][data-listen]';
-		$(selector).each(function() {
-			$(this).dataTable().api().ajax.url(Befound.App.createUrl($(this).data('route'), {'id' : id})).load();
-		})
-	});
-	$(document).on('update.' + ns + ' create.' + ns + ' delete.' + ns, function(event, model, data) {
-		var selector = 'table.dataTable[data-model=' + model + '][data-route][data-listen]';
-		$(selector).each(function() {
-			// Get id.
-			var field = $(this).data('basemodel').toLowerCase() + '_id';
-			if (typeof data.attributes[field] != 'undefined')
-			{
-				$(this).dataTable().api().ajax.url(Befound.App.createUrl($(this).data('route'), {'id' : data.attributes[field]})).load();
-			}
-			else
-			{
-				$(this).dataTable().api().ajax.reload();
-			}
-		})
+if (typeof Befound != 'undefined')
+{
+	Befound.ready(function() {
+		var ns = Befound.App.name;
+		$(document).on('select.' + ns, function(event, model, id) {
+			var selector = 'table.dataTable[data-basemodel=' + model + '][data-route][data-listen]';
+			$(selector).each(function() {
+				$(this).dataTable().api().ajax.url(Befound.App.createUrl($(this).data('route'), {'id' : id})).load();
+			})
+		});
+		$(document).on('update.' + ns + ' create.' + ns + ' delete.' + ns, function(event, model, data) {
+			var selector = 'table.dataTable[data-model=' + model + '][data-route][data-listen]';
+			$(selector).each(function() {
+				// Get id.
+				var field = $(this).data('basemodel').toLowerCase() + '_id';
+				if (typeof data.attributes[field] != 'undefined')
+				{
+					$(this).dataTable().api().ajax.url(Befound.App.createUrl($(this).data('route'), {'id' : data.attributes[field]})).load();
+				}
+				else
+				{
+					$(this).dataTable().api().ajax.reload();
+				}
+			})
 
+		});
 	});
-});
+}
